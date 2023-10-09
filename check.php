3b88a5a5deaa979dc311a22752507909ecf1f10a
null-enc <null-enc@null>
2023-10-10 01:22:30 +0200

    1.0

--- check.php
+++ check.php
@@ -9,14 +9,7 @@ $key = $_POST['key'];
 $logins = file_get_contents("./config/logins.json");
 $logins_parsed = json_decode($logins, true);
 
-print_r($logins_parsed);
-// die;
-
-echo "<br>Имя проекта: " . $name . "<br>";
-echo "Ключ: " . $key . "<br>";
-
 foreach($logins_parsed as $data){
-    echo "Проверяем " . $data['name'] . " " . $data['key'] . "<br>";
     if ($data['name'] === $name && $data['key'] === $key){
         $erro = false;
         echo "Найдено совпадение!";
